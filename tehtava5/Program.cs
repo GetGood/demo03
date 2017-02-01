1592ab834dd123bb0162fc2d0c6fd3cea4dc9f54
k2851 <k2851@DESKTOP-9J2R0SN>
2017-02-01 22:31:38 +0200

    tehtava5 teehty

---
+++ tehtava5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array1 = new object[3];
+
+            Students student1 = new Students("Jorma");
+            student1.Lastname = "Jorakainen";
+            student1.Age = 21;
+            student1.StudentID = "00201";
+            student1.TakeCourse();
+            array1[0] = student1;
+
+            Students student2 = new Students("Kata");
+            student2.Lastname = "Kaerkkaeinen";
+            student2.Age = 65;
+            student2.StudentID = "41222";
+            student2.TakeCourse();
+            array1[1] = student2;
+
+            Students student3 = new Students("Make");
+            student3.Lastname = "ItStop";
+            student3.Age = 3;
+            student3.StudentID = "27603";
+            student3.TakeCourse();
+            array1[2] = student3;
+
+            foreach (Students student in array1)
+            {
+                student.PrintData();
+            }
+
+        }
+    }
+}
---
+++ tehtava5/Students.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava5
+{
+    class Students
+    {
+        // properties
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string StudentID { get; set; }
+        public int Age { get; set; }
+        public string Courses { get; set; }
+
+        // constructors
+
+        public Students(string firstname)
+        {
+            Firstname = firstname;
+        }
+
+        // methods
+
+        public void PrintData()
+        {
+            Console.WriteLine("Information about the student: ");
+            Console.WriteLine("First name: " + Firstname);
+            Console.WriteLine("Last name: " + Lastname);
+            Console.WriteLine("Student ID: " + StudentID);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Classes: " + Courses);
+        }
+        public void TakeCourse()
+        {
+            Console.WriteLine("Give the name of the course:  type 0 to quit or move to next student");
+            string coursetoadd = Console.ReadLine();
+            while (true){
+                if (coursetoadd != "0")
+                {
+
+                    Courses += coursetoadd;
+                    Courses += ",";
+                    Console.WriteLine("Course added!");
+                    coursetoadd = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("exiting..");
+                    break;
+                }
+            }
+        }
+    }
+}
