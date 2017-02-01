d4ad9a6e11c57c009670b2e1f44be93e9d2372b0
k2851 <k2851@DESKTOP-9J2R0SN>
2017-02-01 20:42:46 +0200

    teht1 done

--- tehtava1/Heater.cs
+++ tehtava1/Heater.cs
@@ -9,7 +9,65 @@ namespace tehtava1
     class Heater
     {
         //properties
-        //constructors
-        //methods
+        public bool PowerSwitch { get; set; }
+        public int Humidity { get; set; }
+        public int Temperature { get; set; }
+        //default constructor
+        public Heater()
+        {
+        }
+        public void WarmUp()
+        {
+            if (Temperature <= 200)
+            {
+                Temperature += 5;
+            }
+            else
+            {
+                Console.WriteLine("Heater at maximum temperature");
+            }
+        }
+        public void CoolDown()
+        {
+            if (Temperature > 0)
+            {
+                Temperature -= 5;
+            }
+            else
+            {
+                Console.WriteLine("Heater at the coldest");
+            }
+        }
+        // methods to control humidity
+        public void HumidityUp()
+        {
+            if (Humidity <= 250)
+            {
+                Humidity += 5;
+            }
+            else
+            {
+                Console.WriteLine("Humidity at maximum");
+            }
+        }
+        public void HumidityDown()
+        {
+            if (Humidity > 0)
+            {
+                Humidity -= 5;
+            }
+            else
+            {
+                Console.WriteLine("Humidity at minium");
+            }
+        }
+        // method to print data
+        public void PrintData()
+        {
+            Console.WriteLine("Heater data ");
+            Console.WriteLine("- On/Off?: " + PowerSwitch);
+            Console.WriteLine("- Temperature: " + Temperature);
+            Console.WriteLine("- Humidity: " + Humidity);
+        }
     }
 }
--- tehtava1/Program.cs
+++ tehtava1/Program.cs
@@ -10,8 +10,25 @@ namespace tehtava1
     {
         static void Main(string[] args)
         {
+
+
+             
             //create one instance of heater class
+            Heater heater = new Heater();
+            heater.PrintData();
             //testing...
+            heater.PowerSwitch = true;
+            heater.HumidityUp();
+            heater.PrintData();
+            heater.HumidityDown();
+            heater.PrintData();
+            heater.WarmUp();
+            heater.PrintData();
+            heater.CoolDown();
+            heater.PrintData();
+            heater.CoolDown();
+            Console.ReadLine();
         }
+
     }
 }
